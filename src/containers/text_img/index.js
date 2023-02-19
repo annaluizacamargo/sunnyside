a9b489e8bed139f8920d4c7d1ae6fe10741ae816
Anna Luiza <luizafistarol@gmail.com>
2023-02-19 20:25:34 -0300

    feat(containers): create TextImg

---
+++ src/containers/text_img/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+const TextImg = () => {
+    console.log('TextImg');
+
+    return (
+        <div className='text-img'>
+
+        </div>
+    );
+};
+
+export default TextImg;
