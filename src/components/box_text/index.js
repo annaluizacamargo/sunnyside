755a1be480fdd3d3b4c7507383828779d1b7d1a5
Anna Luiza <luizafistarol@gmail.com>
2023-02-19 19:14:05 -0300

    feat(BoxText): add HTML

--- src/components/box_text/index.js
+++ src/components/box_text/index.js
@@ -4,7 +4,11 @@ const BoxText = () => {
     console.log('BoxText');
 
     return (
-        <h1>BoxText</h1>
+        <div className='box_text' >
+            <h2>Transform your brand</h2>
+            <p>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
+            <h5>Learn more</h5>
+        </div>
     )
 };
 
