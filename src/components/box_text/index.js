31a572bc8202601634c04ec88946f03dfc6ccd76
Anna Luiza <luizafistarol@gmail.com>
2023-02-19 19:37:42 -0300

    style(BoxText): add style

--- src/components/box_text/index.js
+++ src/components/box_text/index.js
@@ -5,7 +5,7 @@ const BoxText = () => {
 
     return (
         <div className='box_text' >
-            <h2>Transform your brand</h2>
+            <h1>Transform your brand</h1>
             <p>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
             <h5>Learn more</h5>
         </div>
