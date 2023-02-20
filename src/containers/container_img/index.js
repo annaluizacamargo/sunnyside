6268e6c6668529bc923ce590f31e953dd0363e20
Anna Luiza <luizafistarol@gmail.com>
2023-02-19 21:46:22 -0300

    feat(ContainerImg): add HTML

--- src/containers/container_img/index.js
+++ src/containers/container_img/index.js
@@ -1,11 +1,22 @@
+import BoxDual from '../../components/box_dual';
 import './style.css';
 
 const ContainerImg = () => {
-    console.log('ContainerImg');
-
     return (
         <div className='container-img'>
-
+            <BoxDual
+                title='Graphic design'
+                description='Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.'
+                color='cyan-dark'
+                img='../../../images/desktop/image-graphic-design.jpg'
+            />
+            
+            <BoxDual
+                title='Graphic design'
+                description='Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.'
+                color='blue-dark'
+                img='../../../images/desktop/image-photography.jpg'
+            />
         </div>
     );
 };
