ff550b97ceeb5018fe48be996094c1542ba18108
Anna Luiza <luizafistarol@gmail.com>
2023-02-20 12:37:30 -0300

    style(BoxLogo): edit style

--- src/containers/box_logo/index.js
+++ src/containers/box_logo/index.js
@@ -1,13 +1,12 @@
 import './style.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDownLong } from '@fortawesome/free-solid-svg-icons';
-
 
 const BoxLogo = () => {
     return (
         <div className='box-logo'>
-            <h1>We are creatives</h1>
-            <FontAwesomeIcon icon={faArrowDownLong} />
+            <div>
+                <h1>We are creatives</h1>
+                <img src='../../../images/icon-arrow-down.svg' className='icon'></img>
+            </div>
             <img src='../../../images/desktop/image-header.jpg'></img>
         </div>
     );
