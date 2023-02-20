b3d2340057004de27f89e676afadcc93fd7d1439
Anna Luiza <luizafistarol@gmail.com>
2023-02-19 23:58:40 -0300

    feat(Person): edit style

--- src/containers/box_testimonials/index.js
+++ src/containers/box_testimonials/index.js
@@ -4,26 +4,30 @@ import './style.css';
 const BoxTestimonials = () => {
     return (
         <div className='box-testimonials'>
-            <Person
-                img='../../../images/image-emily.jpg'
-                text='We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.'
-                name='Emily R.'
-                function='Marketing Director'
-            />
+            <h5>Client testimonials</h5>
+            <div>
+                <Person
+                    img='../../../images/image-emily.jpg'
+                    text='We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.'
+                    name='Emily R.'
+                    function='Marketing Director'
+                />
 
-            <Person
-                img='../../../images/image-thomas.jpg'
-                text='Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.'
-                name='Thomas S.'
-                function='Chief Operating Officer'
-            />
+                <Person
+                    img='../../../images/image-thomas.jpg'
+                    text='Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.'
+                    name='Thomas S.'
+                    function='Chief Operating Officer'
+                />
 
-            <Person
-                img='../../../images/image-jennie.jpg'
-                text='Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!'
-                name='Jennie F.'
-                function='Business Owner'
-            />
+                <Person
+                    img='../../../images/image-jennie.jpg'
+                    text='Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!'
+                    name='Jennie F.'
+                    function='Business Owner'
+                />
+
+            </div>
         </div>
     );
 };
