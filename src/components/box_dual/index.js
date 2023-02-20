aff576b5336db3effa4449edbc34bc5f1eabf090
Anna Luiza <luizafistarol@gmail.com>
2023-02-19 21:54:58 -0300

    feat(BoxDual): add props color background

--- src/components/box_dual/index.js
+++ src/components/box_dual/index.js
@@ -4,19 +4,21 @@ const BoxDual = (props) => {
     const title = props.title;
     const description = props.description;
     const textColor = props.color;
+    const backgroundColorImage = props.backgroundColor;
     const imageBackground = props.img;
 
     const styleBox = {
+        backgroundColor: backgroundColorImage,
         backgroundImage: `url(${imageBackground})`,
         color: `var(--${textColor}`,
-    }
+    };
 
     return (
         <div className='box-dual' style={styleBox}>
             <h2>{title}</h2>
             <p>{description}</p>
         </div>
-    )
+    );
 };
 
 export default BoxDual;
--- src/containers/container_img/index.js
+++ src/containers/container_img/index.js
@@ -8,13 +8,15 @@ const ContainerImg = () => {
                 title='Graphic design'
                 description='Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.'
                 color='cyan-dark'
+                backgroundColor='#8CD4C6'
                 img='../../../images/desktop/image-graphic-design.jpg'
             />
             
             <BoxDual
-                title='Graphic design'
-                description='Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.'
+                title='Photography'
+                description='Increase your credibility by getting the most stunning, high-quality photos that improve your business image.'
                 color='blue-dark'
+                backgroundColor='#5CCAFF'
                 img='../../../images/desktop/image-photography.jpg'
             />
         </div>
