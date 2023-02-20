ac53bc94fc677748e0577609114a2593e6b28e23
Anna Luiza <luizafistarol@gmail.com>
2023-02-19 21:32:38 -0300

    feat(App): add TextImg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import BoxDual from './components/box_dual';
+import BoxText from './components/box_text';
+import TextImg from './containers/text_img';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextImg
+        flex='row'
+        title='Transform your brand'
+        description='We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.'
+        color='yellow'
+        img='../../../images/desktop/image-transform.jpg'
+      />
+
+      <TextImg
+        flex='row-reverse'
+        title='Stand out to the right audience'
+        description='Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.'
+        color='red'
+        img='../../../images/desktop/image-stand-out.jpg'
+      />
     </div>
   );
-}
+};
 
 export default App;
