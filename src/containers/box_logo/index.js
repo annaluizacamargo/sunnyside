d1918208320b47a44f79fb88b9a6ac98a64673e0
Anna Luiza <luizafistarol@gmail.com>
2023-02-20 00:20:34 -0300

    feat(BoxLogo): add icon FontAwensome

--- src/containers/box_logo/index.js
+++ src/containers/box_logo/index.js
@@ -1,9 +1,13 @@
 import './style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDownLong } from '@fortawesome/free-solid-svg-icons';
+
 
 const BoxLogo = () => {
     return (
         <div className='box-logo'>
-            <h1>BoxLogo</h1>
+            <h1>We are creatives</h1>
+            <FontAwesomeIcon icon={faArrowDownLong} />
             <img src='../../../images/desktop/image-header.jpg'></img>
         </div>
     );
