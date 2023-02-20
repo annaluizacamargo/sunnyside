94aeddcb83e99f62e333c0e45a22b7030738b4a4
Anna Luiza <luizafistarol@gmail.com>
2023-02-19 21:35:32 -0300

    feat(components): create ContainerImg

---
+++ src/containers/container_img/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+const ContainerImg = () => {
+    console.log('ContainerImg');
+
+    return (
+        <div className='container-img'>
+
+        </div>
+    );
+};
+
+export default ContainerImg;
