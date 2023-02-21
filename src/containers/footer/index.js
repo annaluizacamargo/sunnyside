b0c0ece82e9ece53f2288ab94291799512fa3077
Anna Luiza <luizafistarol@gmail.com>
2023-02-21 20:37:16 -0300

    feat(Footer): add HTML

--- src/containers/footer/index.js
+++ src/containers/footer/index.js
@@ -1,10 +1,24 @@
 import './style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare, faInstagram, faTwitter, faPinterest } from '@fortawesome/free-brands-svg-icons';
+
 
 const FooterBox = () => {
-    console.log('FooterBox');
-
     return (
-        <h1>FooterBox</h1>
+        <footer className='footer-box'>
+            <h1>sunnyside</h1>
+            <ul className='page'>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Projects</li>
+            </ul>
+            <div className='contact'>
+                <FontAwesomeIcon icon={faFacebookSquare} />
+                <FontAwesomeIcon icon={faInstagram} />
+                <FontAwesomeIcon icon={faTwitter} />
+                <FontAwesomeIcon icon={faPinterest} />
+            </div>
+        </footer>
     )
 };
 
