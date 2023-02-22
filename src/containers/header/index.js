1f2acbbee11e6d7b6cb61da99d5e3a9a6095ff98
Anna Luiza <luizafistarol@gmail.com>
2023-02-22 00:31:51 -0300

    style(Header): add event btnHamburguer

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -1,22 +1,36 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './style.css';
 
-const HeaderBox = (props) => {
-    const typeDisplay = props.typeDisplay;
+const HeaderBox = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    function showMenuDisplay() {
+        setShowMenu(!showMenu);
+        const menuAba = document.getElementById('arrow-up');
+        const menu = document.getElementById('ul-menu');
+
+        if (showMenu) {
+            menuAba.style.display = 'block';
+            menu.style.display = 'inline-block';
+        } else {
+            menuAba.style.display = 'none';
+            menu.style.display = 'none';
+        };
+    };
 
     return (
         <header>
             <div className='header-box'>
                 <h1>Sunnyside</h1>
 
-                <button className='btn-hamburguer'>
+                <button className='btn-hamburguer' onClick={showMenuDisplay}>
                     <img src='../../../images/icon-hamburger.svg'></img>
                 </button>
             </div>
-            
-            <div class="arrow-up"></div>
-            
-            <ul className='ul-menu'>
+
+            <div id="arrow-up"></div>
+
+            <ul id='ul-menu'>
                 <li>About</li>
                 <li>Services</li>
                 <li>Projects</li>
