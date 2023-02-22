81259409f4a14d485cb528fffc08d8732b9052b2
Anna Luiza <luizafistarol@gmail.com>
2023-02-21 22:02:48 -0300

    style(BoxDual): add responsive style

--- src/containers/container_img/index.js
+++ src/containers/container_img/index.js
@@ -1,7 +1,9 @@
 import BoxDual from '../../components/box_dual';
 import './style.css';
 
-const ContainerImg = () => {
+const ContainerImg = (props) => {
+    const typeDisplay = props.typeDisplay;
+
     return (
         <div className='container-img'>
             <BoxDual
@@ -9,15 +11,15 @@ const ContainerImg = () => {
                 description='Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.'
                 color='cyan-dark'
                 backgroundColor='#8CD4C6'
-                img='../../../images/desktop/image-graphic-design.jpg'
+                img={`../../../images/${typeDisplay}/image-graphic-design.jpg`}
             />
-            
+
             <BoxDual
                 title='Photography'
                 description='Increase your credibility by getting the most stunning, high-quality photos that improve your business image.'
                 color='blue-dark'
                 backgroundColor='#5CCAFF'
-                img='../../../images/desktop/image-photography.jpg'
+                img={`../../../images/${typeDisplay}/image-photography.jpg`}
             />
         </div>
     );
