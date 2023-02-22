1527ebdb6dbd74b5329f26512a6252bef55860f3
Anna Luiza <luizafistarol@gmail.com>
2023-02-22 00:16:05 -0300

    style(Header): add responsive style and create btnHamburguer

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -1,16 +1,28 @@
+import { useEffect, useState } from 'react';
 import './style.css';
 
-const HeaderBox = () => {
+const HeaderBox = (props) => {
+    const typeDisplay = props.typeDisplay;
+
     return (
-        <div className='header-box'>
-            <h1>Sunnyside</h1>
-            <ul>
+        <header>
+            <div className='header-box'>
+                <h1>Sunnyside</h1>
+
+                <button className='btn-hamburguer'>
+                    <img src='../../../images/icon-hamburger.svg'></img>
+                </button>
+            </div>
+            
+            <div class="arrow-up"></div>
+            
+            <ul className='ul-menu'>
                 <li>About</li>
                 <li>Services</li>
                 <li>Projects</li>
                 <li>Contact</li>
             </ul>
-        </div>
+        </header>
     );
 };
 
