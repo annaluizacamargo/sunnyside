6f31d4d79876428aa0092a5196a28c61120292a5
Anna Luiza <luizafistarol@gmail.com>
2023-02-21 21:23:35 -0300

    style(BoxLogo): add responsive style

--- src/containers/box_logo/index.js
+++ src/containers/box_logo/index.js
@@ -1,13 +1,16 @@
 import './style.css';
+import * as React from 'react';
 
-const BoxLogo = () => {
+const BoxLogo = (props) => {
+    const typeDisplay = props.typeDisplay;
+
     return (
         <div className='box-logo'>
             <div>
                 <h1>We are creatives</h1>
                 <img src='../../../images/icon-arrow-down.svg' className='icon'></img>
             </div>
-            <img src='../../../images/desktop/image-header.jpg'></img>
+            <img src={`../../../images/${typeDisplay}/image-header.jpg`}></img>
         </div>
     );
 };
