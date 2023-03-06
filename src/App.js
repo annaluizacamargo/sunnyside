e93ed4bbb3bae2aecd9a233167a5a6c60c0eff83
Anna Luiza <luizafistarol@gmail.com>
2023-03-06 12:22:52 -0300

    feat: rename import

--- src/App.js
+++ src/App.js
@@ -1,62 +1,62 @@
 import './App.css';
 import * as React from 'react';
-import Header from './containers/header';
-import AboutSection from './containers/about_section';
-import ServicesSection from './containers/services_section';
-import TestimonialsSection from './containers/testimonials_section';
-import ProjectsSection from './containers/projects_section';
-import ContactSection from './containers/contact_section';
+import Header from './pages/header';
+import AboutSection from './pages/about_section';
+import ServicesSection from './pages/services_section';
+import TestimonialsSection from './pages/testimonials_section';
+import ProjectsSection from './pages/projects_section';
+import ContactSection from './pages/contact_section';
 
 function App() {
-  const [typeDisplay, setTypeDisplay] = React.useState('');
-  const breakpoint = 999;
+	const [typeDisplay, setTypeDisplay] = React.useState('');
+	const breakpoint = 999;
 
-  React.useEffect(() => {
-    window.innerWidth > breakpoint ? setTypeDisplay('desktop') : setTypeDisplay('mobile');
+	React.useEffect(() => {
+		window.innerWidth > breakpoint ? setTypeDisplay('desktop') : setTypeDisplay('mobile');
 
-    window.addEventListener("resize", () => {
-      window.innerWidth > breakpoint ? setTypeDisplay('desktop') : setTypeDisplay('mobile');
-    });
-  }, []);
+		window.addEventListener("resize", () => {
+			window.innerWidth > breakpoint ? setTypeDisplay('desktop') : setTypeDisplay('mobile');
+		});
+	}, []);
 
-  const handleClickScroll = (event) => {
-    const elementClick = event.target.textContent.toLowerCase();
-    const about = document.getElementById('about');
-    const services = document.getElementById('services');
-    const projects = document.getElementById('projects');
-    const contact = document.getElementById('contact');
+	const handleClickScroll = (event) => {
+		const elementClick = event.target.textContent.toLowerCase();
+		const about = document.getElementById('about');
+		const services = document.getElementById('services');
+		const projects = document.getElementById('projects');
+		const contact = document.getElementById('contact');
 
-    switch (true) {
-      case (elementClick == 'about'):
-        about.scrollIntoView({ behavior: 'smooth' });
-        break;
-      case (elementClick == 'services'):
-        services.scrollIntoView({ block: "start", behavior: 'smooth' });
-        break;
-      case (elementClick == 'projects'):
-        projects.scrollIntoView({ behavior: 'smooth' });
-        break;
-      case (elementClick == 'contact'):
-        contact.scrollIntoView({ behavior: 'smooth' });
-        break;
-    };
-  };
+		switch (true) {
+			case (elementClick == 'about'):
+				about.scrollIntoView({ behavior: 'smooth' });
+				break;
+			case (elementClick == 'services'):
+				services.scrollIntoView({ block: "start", behavior: 'smooth' });
+				break;
+			case (elementClick == 'projects'):
+				projects.scrollIntoView({ behavior: 'smooth' });
+				break;
+			case (elementClick == 'contact'):
+				contact.scrollIntoView({ behavior: 'smooth' });
+				break;
+		};
+	};
 
-  return (
-    <div className="App">
-      <Header typeDisplay={typeDisplay} handleClickScroll={handleClickScroll} />
+	return (
+		<div className="App">
+			<Header typeDisplay={typeDisplay} handleClickScroll={handleClickScroll} />
 
-      <AboutSection typeDisplay={typeDisplay} />
+			<AboutSection typeDisplay={typeDisplay} />
 
-      <ServicesSection typeDisplay={typeDisplay} />
+			<ServicesSection typeDisplay={typeDisplay} />
 
-      <TestimonialsSection />
+			<TestimonialsSection />
 
-      <ProjectsSection typeDisplay={typeDisplay} />
+			<ProjectsSection typeDisplay={typeDisplay} />
 
-      <ContactSection typeDisplay={typeDisplay} handleClickScroll={handleClickScroll} />
-    </div>
-  );
+			<ContactSection typeDisplay={typeDisplay} handleClickScroll={handleClickScroll} />
+		</div>
+	);
 };
 
 export default App;
