3a1cf9e35c3f3108a12d452ed80539ddbe168f05
Anna Luiza <luizafistarol@gmail.com>
2023-03-05 23:21:10 -0300

    style(services_section): edit style from frontendmentor

--- src/containers/services_section/index.js
+++ src/containers/services_section/index.js
@@ -7,18 +7,16 @@ const ServicesSection = (props) => {
     return (
         <div id='services' className='container-img'>
             <ContainerTextWithImageBackground
-                title='Graphic design'
+                title='Graphic Design'
                 description='Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.'
-                color='cyan-dark'
-                backgroundColor='#8CD4C6'
+                color='var(--dark-desaturated-cyan)'
                 img={`../../../images/${typeDisplay}/image-graphic-design.jpg`}
             />
 
             <ContainerTextWithImageBackground
                 title='Photography'
                 description='Increase your credibility by getting the most stunning, high-quality photos that improve your business image.'
-                color='blue-dark'
-                backgroundColor='#5CCAFF'
+                color='var(--dark-blue)'
                 img={`../../../images/${typeDisplay}/image-photography.jpg`}
             />
         </div>
